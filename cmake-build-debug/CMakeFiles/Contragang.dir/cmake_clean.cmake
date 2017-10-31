a7a6ac26a133f48854e17b5879562d5f38d5e19d
mohamed <msalad@uncc.edu>
2017-10-31 00:14:29 -0400

    hi

---
+++ cmake-build-debug/CMakeFiles/Contragang.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Contragang.dir/main.c.o"
+  "Contragang.pdb"
+  "Contragang"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Contragang.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Contragang.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Contragang.
+# This may be replaced when dependencies are built.
